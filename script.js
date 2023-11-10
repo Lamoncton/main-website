40fbdf1a5e35c529ba41309255e4e20c6eefa485
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-11-10 21:35:42 +0700

    Update preview

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-let COMP_LOAD = document.querySelectorAll("[html-src]").length; // total number of component need to load
-
-(() => {
-    // Load all HTML using fetch, keep for preview but will soon remove after the translation update
-    document.querySelectorAll("[html-src]").forEach(elm => {
-        fetch(elm.getAttribute('html-src')).then(res => res.text())
-        .then((res) => {
-            elm.innerHTML = res + elm.innerHTML; // do not over write file
-            compLoaded();
-        });
-    });
-})();
-
-// wait until all components loaded
-function compLoaded() {
-    COMP_LOAD -= 1;
-    if (COMP_LOAD != 0) return; // if not done loading
-
-    // all components loaded
-}
-
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
